3c92fa2e81fbb8411fc3c798c275bb4a30f9b33e
Zeynebesra <zeynep.ozturk@mobilion.com.tr>
2022-01-21 16:18:31 +0300

    first commit

---
+++ baslangic-seviye/js-egitimi/js/degisken-tanimlama-kurallari.js
@@ -0,0 +1,36 @@
+// ### Degisken Genel Tanimlama Kurallari:
+// camelCase Kullanimi:
+let fullname = ""; // bu dogru kullanim degil :(
+let kodluyoruzserverinfo = ""; // bu dogru kullanim degil :(
+let kodluyoruz_server_info = ""; // bu dogru kullanim degil :(
+let fullName = "";
+let kodluyoruzServerInfo = ""; // bu dogru :)
+let kodluyoruzSERverInfo = ""; // bu dogru kullanim degil :(
+
+// let firstName = "Hakan", lastName = "Yalcinkaya"
+let firstName = "Hakan";
+let lastName = "Yalcinkaya";
+console.log(firstName, lastName);
+
+// UPPER_CASE Kullanimi:
+const password = "1234"; // bu dogru kullanim degil :(
+const PASSWORD = "1234"; // dogru kullanim
+const SERVER_PASSWORD = "1234";
+
+// degiskenlerde turkce ve diger dillerin kullanimi:
+let türkçeBilgi = "Türkçe Bilgi Yazdırılıyor.."; // bu dogru kullanim degil :(
+// let turkceBilgi // olmazzz :(
+let info = "Türkçe Bilgi Yazdırılıyor..";
+console.log(info);
+
+// anlamsiz degisken adlari kullanmayin!
+
+let x = 1; // bu dogru kullanim degil :(
+// x, y, z, k, i, e, gibi anlamsiz degisken isimlerini kullanmayin!
+
+// ### Ek Bilgi:
+// 1: boolean tanimlarken is/has kullanimi:
+let isActive = true;
+let hasPassword = false;
+
+// 2: Line Length < 80
---
+++ baslangic-seviye/js-egitimi/js/degisken-tanimlama.js
@@ -0,0 +1,55 @@
+// ********** let ve const ile Degisken Tanimlama **********
+
+// 1.)var ile degisken tanimlamak:
+// var serverName = "api.kodluyoruz.org";
+// console.log(serverName);
+
+// 2.)let ile degiskeni bos tanimlamak:
+let serverName;
+console.log(serverName);
+
+// let ile degiskene bilgi atamak:
+serverName = "https://kodluyoruz.org";
+console.log(serverName);
+
+// let ile degiskene bilgi atayarak tanimlamak:
+let password = "1234";
+console.log(password);
+
+// ! degisken atamasi yapmadan once kullanmaya calismak:
+
+/* hatalı kullanım dikkat et !  
+
+console.log(fullName)
+let fullName = "Hakan Yalcinkaya";
+
+*/
+
+let fullName = "ZeyNeb Öztürk";
+console.log(fullName);
+
+// let ile tanimlanan degiskenin icindeki bilgiyi degistirmek:
+
+// fullName = "Lorem Ipsum Dolor"
+// console.log(fullName)
+
+// birlestirme veya ekleme islemi
+// fullName + "Yeni Eklenen Bilgi" // acaba eklendimi ??
+// console.log(fullName + " Test Bilgisi") // Ekle ve Goster ama Degiskene Eklemedik
+
+// fullName = fullName + " Yeni Bilgi"
+// fullName =  "2: Bilgi : " + fullName
+
+// fullName = "Sifirlandi"
+// fullName += " ve Yeni Bilgi Eklendi"
+// console.log(fullName)
+
+// const ile degiskeni bos tanimlamaya calismak :( :
+// const serverPassword; // sadece degisken tanimlandi ama ici bos ??
+
+// const ile degisken tanimlamak:
+// const SERVER_PASSWORD = "oisjefo9jwes221"
+// console.log(SERVER_PASSWORD)
+
+// SERVER_PASSWORD = "1234"
+// console.log(SERVER_PASSWORD)
